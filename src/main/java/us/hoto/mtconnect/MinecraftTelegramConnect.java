f79fa7d68be5bc934bf1fb76003dfcebec12ce1b
Cocoa Hoto <cocoa@hoto.us>
2021-02-25 21:07:13 +0900

    Bump version

--- src/main/java/us/hoto/mtconnect/MinecraftTelegramConnect.java
+++ src/main/java/us/hoto/mtconnect/MinecraftTelegramConnect.java
@@ -34,7 +34,7 @@ import java.util.*;
 public class MinecraftTelegramConnect {
 	public static final String MODID = "mtconnect";
 	public static final String NAME = "Minecraft-Telegram Connect";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 
 	private static boolean active = true;
 
